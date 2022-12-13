9426ad43265999328d80ba949b5bb4d2df00f75d
rbroc <rbrrcc@gmail.com>
2022-12-13 11:43:08 -0600

    update scripts

--- fit_tweetopic.py
+++ fit_tweetopic.py
@@ -3,65 +3,96 @@ from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 from contextualized_topic_models.evaluation.measures import CoherenceNPMI
 import json
+import argparse
+import glob
+from pathlib import Path
 
-def main():
-    topic_df = pd.read_json('data/EU_Commission.jsonl', 
-                            orient='records', 
-                            lines=True)
+parser = argparse.ArgumentParser()
+parser.add_argument('--eucomm-only', type=int, default=1)
+
+
+def main(eucomm_only):
+    if eucomm_only is False:
+        fs = glob.glob('data/derivatives/*')
+    else:
+        fs = glob.glob('data/derivatives/EU_Comm*')
+    dfs = []
+    for f in fs:
+        dfs.append(pd.read_json(f, orient='records', lines=True))
+    topic_df = pd.concat(dfs)
+    
     train_texts = topic_df[topic_df['topic_split']=='train'].text.tolist()
     val_texts = topic_df[topic_df['topic_split']=='val'].text.tolist()
     test_texts = topic_df[topic_df['topic_split']=='test'].text.tolist()
     
     scores = []
-    for min_df in [10, 100]:
-        for max_df in [0.01, 0.1, 0.2]:
-            for n_clusters in [10, 15, 20, 30]:
-                for max_features in [100, 250, 500]:
+    for run in range(5):
+        for min_df in [10, 100]:
+            for max_df in [0.01, 0.1, 0.2]:
+                for n_clusters in [5, 10, 15, 20, 30]:
+                    for max_features in [100, 250, 500]:
 
-                    # Fit
-                    vectorizer = CountVectorizer(min_df=min_df, 
-                                                 max_df=max_df, 
-                                                 max_features=max_features)
-                    dmm = DMM(n_components=n_clusters, 
-                              n_iterations=100, 
-                              alpha=0.1, beta=0.1)
-                    pipeline = TopicPipeline(vectorizer, dmm)
-                    pipeline.fit(train_texts)
+                        # Fit
+                        vectorizer = CountVectorizer(min_df=min_df, 
+                                                     max_df=max_df, 
+                                                     max_features=max_features)
+                        dmm = DMM(n_components=n_clusters, 
+                                  n_iterations=100, 
+                                  alpha=0.1, beta=0.1)
+                        pipeline = TopicPipeline(vectorizer, dmm)
+                        pipeline.fit(train_texts)
 
-                    # Eval
-                    topic_dict = pipeline.top_words(top_n=10)
-                    topics = [list(d.keys()) for d in topic_dict]
-                    topic_save = {i: t for i, t in enumerate(topics)}
-                    id_model = f'comps-{n_clusters}_vocab-{max_features}'
-                    id_model += f'_mindf-{min_df}_maxdf-{max_df}'
-                    json.dump(topic_save, 
-                              open(f'logs/tweetopic/model_{id_model}.json', 'w'))
+                        # Eval
+                        topic_dict = pipeline.top_words(top_n=10)
+                        topics = [list(d.keys()) for d in topic_dict]
+                        topic_save = {i: t for i, t in enumerate(topics)}
+                        id_model = f'comps-{n_clusters}_vocab-{max_features}'
+                        id_model += f'_mindf-{min_df}_maxdf-{max_df}'
+                        if eucomm_only:
+                            OUT_PATH = Path('models') / 'tweetopic' / 'eucomm' / id_model / f'run-{run}'
+                        else:
+                            OUT_PATH = Path('models') / 'tweetopic' / 'all' / id_model / f'run-{run}'
+                        OUT_PATH.mkdir(exist_ok=True, parents=True)
+                        json.dump(topic_save, 
+                                  open(str(OUT_PATH / f'model.json'), 
+                                       'w'))
+                        words = pipeline.vectorizer.get_feature_names_out() 
+                        for split, ds in zip(['train','val','test'],
+                                             [train_texts, val_texts, test_texts]):
+                            out = pipeline.vectorizer.transform(ds)
+                            feats = [] 
+                            for o in out.toarray(): 
+                                f = [words[i] for i in range(len(words)) if o[i]==1]
+                                feats.append(f) 
+                            metric = CoherenceNPMI(topics=topics, texts=feats)
+                            cscore = metric.score()
+                            score_dict = {'name': id_model,
+                                          'split': split,
+                                          'score': cscore}
+                            scores.append(score_dict)
+                        pred_mat = pipeline.transform(topic_df['text'].tolist())
+                        pred_mat = pd.DataFrame(pred_mat, columns=[f'topic_{i}' 
+                                                                   for i in range(n_clusters)])
+                        pred_mat = pd.concat([topic_df, pred_mat], axis=1)
+                        if eucomm_only:
+                            PRED_PATH = Path('logs') / 'tweetopic' / 'eucomm' / id_model / f'run-{run}'
+                        else:
+                            PRED_PATH = Path('logs') / 'tweetopic' / 'all' / id_model / f'run-{run}'
+                        PRED_PATH.mkdir(exist_ok=True, parents=True)
+                        pred_mat.to_json(str(PRED_PATH / f'preds.jsonl'),
+                                         orient='records', 
+                                         lines=True)
 
-                    words = pipeline.vectorizer.get_feature_names_out() 
-                    for split, ds in zip(['train','val','test'],
-                                         [train_texts, val_texts, test_texts]):
-                        out = pipeline.vectorizer.transform(ds)
-                        feats = [] 
-                        for o in out.toarray(): 
-                            f = [words[i] for i in range(len(words)) if o[i]==1]
-                            feats.append(f) 
-                        metric = CoherenceNPMI(topics=topics, texts=feats)
-                        cscore = metric.score()
-                        score_dict = {'name': id_model,
-                                      'split': split,
-                                      'score': cscore}
-                        scores.append(score_dict)
-                    pred_mat = pipeline.transform(topic_df['text'].tolist())
-                    pred_mat = pd.DataFrame(pred_mat, columns=[f'topic_{i}' 
-                                                               for i in range(n_clusters)])
-                    pred_mat = pd.concat([topic_df, pred_mat], axis=1)
-                    pred_mat.to_json(f'logs/tweetopic/predictions_{id_model}.jsonl',
-                                     orient='records', lines=True)
-
-                # Save
-                pd.DataFrame(scores).to_json('logs/tweetopic/performances.jsonl', 
-                                             orient='records', 
-                                             lines=True)
+                    # Save
+                    if eucomm_only:
+                        DF_PATH = Path('logs') / 'tweetopic' / 'eucomm'
+                    else:
+                        DF_PATH = Path('logs') / 'tweetopic' / 'all'
+                    pd.DataFrame(scores).to_json(str(DF_PATH / 'performances.jsonl'), 
+                                                 orient='records', 
+                                                 lines=True)
                 
+
 if __name__=='__main__':
-    main()
+    args = parser.parse_args()
+    main(bool(args.eucomm_only))
--- pretrain.py
+++ pretrain.py
@@ -1,9 +1,21 @@
-from pretrain import Pretrainer
+from src.pretraining import Pretrainer
 import pandas as pd
+from pathlib import Path
+import argparse
+import glob
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--eucomm-only', type=int, default=1)
 
 
-def _save_results(rlist):
-    fname = 'logs/pretraining/performances.jsonl'
+def _save_results(rlist, eucomm_only):
+    fname = Path('logs') / 'pretraining'
+    if eucomm_only:
+         fname = fname / 'eucomm'
+    else:
+         fname = fname / 'all'
+    fname.mkdir(exist_ok=True, parents=True)
+    fname = fname / 'performances.jsonl'
     try:
         rdf = pd.read_json(fname, 
                            orient="records",
@@ -14,21 +26,26 @@ def _save_results(rlist):
     rdf.to_json(fname, orient="records", lines=True)
 
 
-def main():
+def main(eucomm_only):
     
     # Read the data
-    df = pd.read_json('processed/all_tweets.jsonl', lines=True, orient='records')
-    df = df[(df['lang']=='en') & (df['lang_detected']=='en')]
+    if eucomm_only:
+        fs = glob.glob('data/derivatives/EU_Comm*')
+    else:
+        fs = glob.glob('data/derivatives/*')
+    dfs = []
+    for f in fs:
+        df = pd.read_json(f, lines=True, orient='records')
+        dfs.append(df)
+    df = pd.concat(dfs)
     
     # Run pretraining loop
-    models = ['distilbert-base-uncased',
-              'distilbert-base-uncased-finetuned-sst-2-english',
-              'cardiffnlp/tweet-topic-21-multi']
+    models = ['distilbert-base-uncased']
     results = []
 
     # Run entire loop
     for lr in [2e-6, 2e-5, 2e-3]:
-        for batch_size in [4, 16]:
+        for batch_size in [16]:
             for wu_epochs in [3]:
                 for cs in [30, 50, 100]:
                     for m in models:
@@ -40,8 +57,14 @@ def main():
                         trainer.compile()
                         r = trainer.fit()
                         results.append(r)
-                        trainer.save(f'models/pretrained/{trainer.name}')
-                        _save_results(results)   
+                        if eucomm_only:
+                            OUT_PATH = Path('models') / 'pretraining' / 'eucomm'
+                        else:
+                            OUT_PATH = Path('models') / 'pretraining' / 'all'
+                        OUT_PATH.mkdir(exist_ok=True, parents=True)
+                        trainer.save(str(OUT_PATH/f'{trainer.name}'))
+                        _save_results(results, eucomm_only)   
                         
 if __name__=="__main__":
-    main()
+    args = parser.parse_args()
+    main(bool(args.eucomm_only))
